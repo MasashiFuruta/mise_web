0f7f754e60a923bee1b979cfe6e6c584e6034f5f
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-04-29 20:58:00 +0900

    fix devise path

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #namespace :admin do
-  #  resources :unko
-  #end
-
   devise_for :admins, :controllers => {
     :registrations => 'admins/registrations',
     :sessions => 'admins/sessions'
+    :passwords => 'passwords/sessions'
    }
 end
