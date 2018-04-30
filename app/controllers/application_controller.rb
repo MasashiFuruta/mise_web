365f4bc983baba6e2fa0fbb0e2fe85dfec134bd8
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-04-30 11:45:02 +0900

    login and logout redirect setting for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # 管理者ページログイン後に遷移するpathを設定
+  def after_sign_in_path_for(resource)
+    admin_root_path 
+  end
+  # 管理者ページログアウト後に遷移するpathを設定
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
